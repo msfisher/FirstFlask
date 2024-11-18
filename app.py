ce072d306e9e780bfa0eb21123b46e9c5260d451
Marcus Fisher <Marcus.S.Fisher@gmail.com>
2024-11-18 09:31:28 -0500

    added forms

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ If you don't use the file name "app.py" then create an environment variable "FLA
 and set it to the value of your file that you want to be served up.
 """
 # import re
-from flask import Flask
+from flask import Flask, request, render_template
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -14,44 +14,39 @@ app = Flask(__name__)
 
 # decorator is used to connect URL endpoints with code in functions
 # argument to @app.route() defines the URL's path component, which is root in this case
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
     """
     f() is wrapped with a decorator and defines what should be executed if
     the defined URL endpoint is requested by the user
     We have attached this function to the route ("/") which is home page
     """
-    return "Hello, Flask!"
+    return render_template("index.html")
 
 
-@app.route("/about/<name>")
-def about(name):
+@app.route("/read_form", methods=["POST"])
+def read_form():
     """
-    decorator argument "/about/<name>" defines the endpoint "/about/" that can
-    accept any additional value. The identifier within the angled brackets
-    defines a variable that is passed to the function and can be used in the code
-
     Always filter user-provided information to avoid attacks on the app. If you use
     Templates then Flask does this for you automatically
     """
+    # get the forma data
+    data = request.form
 
-    # the following code has been commented out so that a new feature can be added
-    # to ensure secure values are passed to the function.
+    # filter the user data to ensure safe letters are passed into function
+    # filtered_text = re.match("[a-zA-Z, . ']+", data['textSubmit'])
 
-    # filter the name argument to ensure safe letters are passed into function
-    # filtered_arg = re.match("[a-zA-Z, . ']+", name)
-
-    # as long as filtered_arg is not None then get the data
-    #if filtered_arg:
-    #    clean_arg = filtered_arg.group(0)
+    # as long as filtered_text is not None then we can use the data
+    #if filtered_text:
+    #    clean_text = filtered_arg.group(0)
     #else:
-    #    clean_arg = "Something went wrong"
+    #    clean_text = "Something went wrong"
 
-    clean_arg = name
+    clean_text = data['textSubmit']
 
     # send what the user sent to the end point to word frequency function
     # a dictionary is returned
-    return_content = word_frequency(clean_arg.lower())
+    return_content = word_frequency(clean_text.lower())
 
     # send the top used tokens that the user sent back to the browser
     return return_content
